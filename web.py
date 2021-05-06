dd17e75bcc2990e1c7db4a344c7f36eb606dbb6b
Gabriel Freundt <gfreundt@gmail.com>
2021-05-06 16:59:19 -0500

    New

--- web.py
+++ web.py
@@ -1,34 +1,33 @@
 import os
 from flask import Flask, redirect, url_for, render_template, request
-import updater
 import datetime as dt
 import time
 import csv
 
 
 def find_path():
-    paths = [(r'C:\Users\Gabriel Freundt\Webing',r"D:\Webing", r"C:\users\gfreu\Webing"), (r'C:\Users\Gabriel Freundt\Google Drive\Multi-Sync\sharedData',r"D:\Google Drive Backup\Multi-Sync\sharedData", r"C:\users\gfreu\Google Drive\Multi-Sync\sharedData")]
-    for k, path in enumerate(paths[1]):
+    paths = [r'C:\Users\Gabriel Freundt\Google Drive\Multi-Sync\sharedData',r"D:\Google Drive Backup\Multi-Sync\sharedData", r"C:\users\gfreu\Google Drive\Multi-Sync\sharedData"]
+    for path in paths:
         if os.path.exists(path):
-            return paths[0][k], paths[1][k]
+            return path
 
-app_path, data_path = find_path()
-DATA_PATH = os.path.join(data_path, 'data')
-HTML_PATH = os.path.join(app_path)
-CSS_PATH = os.path.join(app_path)
+DATA_PATH = os.path.join(find_path(), 'data')
 
-print(HTML_PATH)
+app = Flask(__name__)
 
-
-app = Flask(__name__) #, template_folder=HTML_PATH) #, static_folder=CSS_PATH)
-
-@app.route("/dog")
+@app.route("/tdc")
 def index():
 	with open(os.path.join(DATA_PATH, 'TDC_fixed.txt'), mode='r') as file:
 		data = [i for i in csv.reader(file, delimiter=',')]
 		promedio, promedio_time, promedio_date = data[0]
 		latest = data[1:]
-		return render_template('index.html', latest=latest, promedio=promedio, promedio_date=promedio_date, promedio_time=promedio_time)
+		images, urls, quotes = [i[0] for i in latest], [i[1] for i in latest], [i[2] for i in latest]
+		images1, images2 = images[:len(images)//2], images[len(images)//2:]
+		quotes1, quotes2 = quotes[:len(quotes)//2], quotes[len(quotes)//2:]
+
+		print(images1,images2)
+		print(quotes1,quotes2)
+		return render_template('index.html', quotes1=quotes1, quotes2=quotes2, images1=images1, images2=images2, urls=urls, promedio=promedio, promedio_date=promedio_date, promedio_time=promedio_time)
 
 #return render_template("dashboard.html", date=date, time=time, data=data)
 
