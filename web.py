8a94ac837bc3283d910ec53cefaf368bf47671be
Gabriel Freundt <gfreundt@gmail.com>
2021-05-28 20:43:08 -0500

    Compra y Venta

--- web.py
+++ web.py
@@ -1,7 +1,5 @@
 import os
 from flask import Flask, redirect, url_for, render_template, request
-import datetime as dt
-import time
 import json
 
 
@@ -16,12 +14,25 @@ DATA_PATH = os.path.join(find_path(), 'data')
 app = Flask(__name__)
 
 @app.route("/tdc")
-def index():
+def tdc():
+	return "<a href='http://190.117.62.94:65001/venta' <b>Esta página ha cambiado de dirección</b></p></a>"
+
+@app.route("/venta")
+def venta():
 	with open(os.path.join(DATA_PATH, 'WEB_Venta.json'), mode='r') as file:
 		data = json.load(file)
 		details = data['details']
 		details1, details2 = details[:len(details)//2], details[len(details)//2:]
-		return render_template('index.html', head=data['head'], details1=details1, details2=details2)
+		return render_template('venta.html', head=data['head'], details1=details1, details2=details2)
+
+@app.route("/compra")
+def compra():
+	with open(os.path.join(DATA_PATH, 'WEB_Compra.json'), mode='r') as file:
+		data = json.load(file)
+		details = data['details']
+		details1, details2 = details[:len(details)//2], details[len(details)//2:]
+		return render_template('compra.html', head=data['head'], details1=details1, details2=details2)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
